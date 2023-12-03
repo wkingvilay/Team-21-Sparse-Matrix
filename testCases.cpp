c9d60ee4136da76abd978b660369ff0f78744674
camhuri <123994913+camhuri@users.noreply.github.com>
2023-12-03 00:08:22 -0500

    Describe each test case in console

--- testCases.cpp
+++ testCases.cpp
@@ -10,10 +10,12 @@ void printMatrix(SparseMatrix matrix);
 int main(int argc, char*argv[]) {
     // First matrix
     SparseMatrix matrix = SparseMatrix();
+    std::cout << "Blank matrix:";
     printMatrix(matrix);
     matrix.setNode(1, 0, 25);
     matrix.setNode(1, 1, 50);
     matrix.setNode(2, 2, 111);
+    std::cout << "Matrix 1:";
     printMatrix(matrix);
 
     // Second matrix
@@ -22,10 +24,12 @@ int main(int argc, char*argv[]) {
     matrix2.setNode(1, 1, 15);
     matrix2.setNode(2, 0, 5);
     matrix2.setNode(0, 2, 5);
+    std::cout << "Matrix 2:";
     printMatrix(matrix2);
 
     // Sum the two matrices
     matrix = matrix.sumMatrix(&matrix2);
+    std::cout << "Matrix 1 + Matrix 2:";
     printMatrix(matrix);
 
     // Test deletion in the middle of a matrix
@@ -35,8 +39,10 @@ int main(int argc, char*argv[]) {
     matrix3.setNode(1, 2, 5);
     matrix3.setNode(2, 1, 5);
     matrix3.setNode(1, 1, 25);
+    std::cout << "Matrix 3:";
     printMatrix(matrix3);
     matrix3.removeNode(1, 1);
+    std::cout << "Matrix 3 after middle node deletion:";
     printMatrix(matrix3);
 
     // Test matrix multiplication
@@ -46,6 +52,7 @@ int main(int argc, char*argv[]) {
     matrix4.setNode(1, 0, 4);
     matrix4.setNode(1, 1, 5);
     matrix4.setNode(1, 2, 6);
+    std::cout << "Matrix 4:";
     printMatrix(matrix4);
     SparseMatrix matrix5 = SparseMatrix(7);
     matrix5.setNode(0, 1, 8);
@@ -53,17 +60,21 @@ int main(int argc, char*argv[]) {
     matrix5.setNode(1, 1, 10);
     matrix5.setNode(2, 0, 11);
     matrix5.setNode(2, 1, 12);
+    std::cout << "Matrix 5:";
     printMatrix(matrix5);
-    matrix4 = matrix4.multiplyMatrix(&matrix5); // matrix4 x matrix5
+    matrix4 = matrix4.multiplyMatrix(&matrix5);
+    std::cout << "Matrix 4 * Matrix 5:";
     printMatrix(matrix4);
 
     // Test reading matrices from file
-    SparseMatrix matrix6 = SparseMatrix("matrix1.txt");
-    printMatrix(matrix6);
-    SparseMatrix matrix7 = SparseMatrix("matrix2.txt");
-    printMatrix(matrix7);
+    SparseMatrix filematrix1 = SparseMatrix("testmatrix1.txt");
+    std::cout << "File test matrix 1:";
+    printMatrix(filematrix1);
+    SparseMatrix filematrix2 = SparseMatrix("testmatrix2.txt");
+    std::cout << "File test matrix 2:";
+    printMatrix(filematrix2);
 }
 
 void printMatrix(SparseMatrix matrix) {
-    std::cout << "Matrix:\n" << matrix.showMatrix() << "\n";
+    std::cout << std::endl << matrix.showMatrix() << std::endl;
 }
